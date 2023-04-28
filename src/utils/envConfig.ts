a3b8bc93165a3162a390224619ac161536a0b03f
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2023-04-28 14:19:29 +0530

    Codegen and react-query setup

---
+++ src/utils/envConfig.ts
@@ -0,0 +1,25 @@
+if (!process.env.NODE_ENV) {
+  require('dotenv').config();
+}
+
+let serverUrl = 'http://localhost:4000';
+
+switch (process.env.REACT_NATIVE_APP_PUBLIC_ENV) {
+  case 'base':
+    serverUrl = 'https://company-portal.onrender.com';
+    break;
+
+  case 'beta':
+    serverUrl = 'https://company-portal-beta.onrender.com';
+    break;
+
+  default:
+    break;
+}
+
+const envConfig = {
+  SERVER_URL: serverUrl,
+  ENV: process.env.REACT_NATIVE_APP_PUBLIC_ENV,
+};
+
+export default envConfig;
