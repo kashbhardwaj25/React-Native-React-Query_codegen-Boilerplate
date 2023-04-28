c7cd6bcecbc80ab558e844c9abc444e9251a1faf
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2023-04-28 16:19:33 +0530

    React native navigation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import {SafeAreaView, Text, View} from 'react-native';
+import {Button, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
-function App(): JSX.Element {
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({navigation}: any) => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Glue Labs Portal</Text>
-      </View>
-    </SafeAreaView>
+    <Button
+      title="Go to Aakash's profile"
+      onPress={() => navigation.navigate('Profile', {name: 'Aakash'})}
+    />
   );
-}
+};
+const ProfileScreen = ({navigation, route}: any) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+const App = () => {
+  const queryClient = new QueryClient();
+
+  return (
+    <NavigationContainer>
+      <QueryClientProvider client={queryClient}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{title: 'Welcome'}}
+          />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
+      </QueryClientProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
