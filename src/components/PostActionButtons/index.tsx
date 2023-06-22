239f2b922f6ab32d58278b5c898a4125497385d2
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2023-06-22 17:29:20 +0530

    Post Action Buttons updated

--- src/components/PostActionButtons/index.tsx
+++ src/components/PostActionButtons/index.tsx
@@ -1,13 +1,35 @@
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
-import AppText from '../shared/AppText'
+import Like from '../../assets/icons/Like'
+import Repost from '../../assets/icons/Repost'
+import Comment from '../../assets/icons/Comment'
 
 const PostActionButtons = () => {
   return (
-    <View>
-      <AppText>Post Action Buttons!</AppText>
+    <View style={styles.postActionsWrapper}>
+      <View style={styles.iconWrapper}>
+        <Like />
+      </View>
+      <View style={styles.iconWrapper}>
+        <Repost />
+      </View>
+      <View style={styles.iconWrapper}>
+        <Comment />
+      </View>
     </View>
   )
 }
 
 export default PostActionButtons
+
+const styles = StyleSheet.create({
+  postActionsWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: 12,
+    justifyContent: 'flex-end',
+  },
+  iconWrapper: {
+    minWidth: 50,
+  },
+})
