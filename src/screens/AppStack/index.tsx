5e25a6aa031a285774ea49f0854389fe5b983faf
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2023-06-27 17:54:49 +0530

    Logout button added in Navigation drawer

--- src/screens/AppStack/index.tsx
+++ src/screens/AppStack/index.tsx
@@ -1,15 +1,35 @@
-import { createDrawerNavigator } from '@react-navigation/drawer'
+import { DrawerContentScrollView, DrawerItem, DrawerItemList, createDrawerNavigator } from '@react-navigation/drawer'
 
 import Home from './Home'
 import Profile from './Profile'
 import Account from './Account'
 import Notifications from './Notifications'
+import useUserStore from '../../store/userStore'
+import { resetTokens } from '../../utils/tokenHelper'
 
 const Drawer = createDrawerNavigator()
 
 const AppStack = () => {
+  const { setIsLoggedIn } = useUserStore()
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      initialRouteName="Home"
+      drawerContent={(props) => {
+        return (
+          <DrawerContentScrollView {...props}>
+            <DrawerItemList {...props} />
+            <DrawerItem
+              label="Logout"
+              onPress={async () => {
+                await resetTokens()
+                setIsLoggedIn(false)
+              }}
+            />
+          </DrawerContentScrollView>
+        )
+      }}
+    >
       <Drawer.Screen name="Home" component={Home} />
       <Drawer.Screen name="Profile" component={Profile} />
       <Drawer.Screen name="Notifications" component={Notifications} />
